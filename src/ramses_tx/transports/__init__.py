90fc117f3fbd470e8ac8823e219882427f83058d
PWhite-Eng <75939018+PWhite-Eng@users.noreply.github.com>
2026-02-18 13:49:43 +0000

    WIP: updated src/ramses_tx/transports/__init__.py after rebase

--- src/ramses_tx/transports/__init__.py
+++ src/ramses_tx/transports/__init__.py
@@ -35,49 +35,11 @@ For re-flashing evofw3 via Arduino IDE on *my* atmega32u4 (YMMV):
   - Pinout:     Pro Micro
 """
 
-"""RAMSES RF - Transports package.
-Operates at the pkt layer of: app - msg - pkt - h/w
-
-For ser2net, use the following YAML with: ``ser2net -c misc/ser2net.yaml``
-
-.. code-block::
-
-    connection: &con00
-    accepter: telnet(rfc2217),tcp,5001
-    timeout: 0
-    connector: serialdev,/dev/ttyUSB0,115200n81,local
-    options:
-        max-connections: 3
-
-For ``socat``, see:
-
-.. code-block::
-
-    socat -dd pty,raw,echo=0 pty,raw,echo=0
-    python client.py monitor /dev/pts/0
-    cat packet.log | cut -d ' ' -f 2- | unix2dos > /dev/pts/1
-
-For re-flashing evofw3 via Arduino IDE on *my* atmega328p (YMMV):
-
-  - Board:      atmega328p (SW UART)
-  - Bootloader: Old Bootloader
-  - Processor:  atmega328p (5V, 16 MHz)
-  - Host:       57600 (or 115200, YMMV)
-  - Pinout:     Nano
-
-For re-flashing evofw3 via Arduino IDE on *my* atmega32u4 (YMMV):
-
-  - Board:      atmega32u4 (HW UART)
-  - Processor:  atmega32u4 (5V, 16 MHz)
-  - Pinout:     Pro Micro
-"""
-
 from __future__ import annotations
 
 import asyncio
 import logging
 from collections.abc import Awaitable, Callable
-from typing import TYPE_CHECKING, Any
 from typing import TYPE_CHECKING, Any
 
 from .. import exceptions as exc
